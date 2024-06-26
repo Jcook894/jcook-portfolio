077bb8bd25a18c0107951adcc994ac3340a477a1
Julian Cook <juliancook@ip-10-0-0-137.ec2.internal>
2024-06-26 17:38:54 -0400

    added mailto logic for contact form, hooked up state.

--- src/components/contact/ContactMe.tsx
+++ src/components/contact/ContactMe.tsx
@@ -31,8 +31,21 @@ export function ContactMe() {
 
     const toggleForm = () => setIsOpen(!isOpen)
 
-    // const setContactForm = ( formField: string ) => setContactForm({ ...contactForm, "form" })
+    const handleChange = ( event: any, field: string ) => {
 
+        const val = event.target.value
+        const err = ( val === "" ? true : false )
+
+        // Validate on input change:
+        setContactForm( {  ...contactForm, [ field ]: { value: event.target.value, error: err } })
+
+    }
+
+    const onSubmit = (e: React.FormEvent) => {
+        e.preventDefault()
+        location.href = `mailto:${contactForm.email.value}?subject=${contactForm.name.value}&body=${contactForm.message.value}`;
+    }
+    
     return (
 
         <div className="my-2 sm:my-4 md:my-6 shadow-sm cursor-pointer">
@@ -45,35 +58,40 @@ export function ContactMe() {
                 <IoIosArrowDown style = {{transform: isOpen ? 'rotate(180deg)' : ''}}/>
             </div>
 
-            <form className={`flex flex-col justify-center px-6 pt-0 overflow-hidden transition-[max-height] duration-200 ease-in ${isOpen ? "max-h-80" : "max-h-0"} w-full`}>
+            <form
+                onSubmit={ (e) => onSubmit(e) }
+                className={`flex flex-col justify-center px-6 pt-0 overflow-hidden transition-[max-height] duration-200 ease-in ${isOpen ? "max-h-80" : "max-h-0"} w-full`}
+            >
                 <div className="flex p-2 w-full rounded-sm">
                     <input
                         type="text"
-                        className="mt-1 w-full rounded-sm border-gray-300 shadow-sm focus:border-purple-500 focus:ring focus:ring-purple-500 focus:ring-opacity-50"
+                        className="text-black mt-1 w-full rounded-sm border-gray-300 shadow-sm focus:border-purple-500 focus:ring focus:ring-purple-500 focus:ring-opacity-50"
                         placeholder="Name"
-    
+                        value={ contactForm.name.value }
+                        onChange={ ( e ) => handleChange( e, "name" ) }
                     />                     
                 </div>
                 <div className="flex p-2">
                     <input
                         type="email"
-                        className="mt-1 w-full rounded-sm border-gray-300 shadow-sm focus:border-purple-500 focus:ring focus:ring-purple-500 focus:ring-opacity-50"
+                        className="text-black mt-1 w-full rounded-sm border-gray-300 shadow-sm focus:border-purple-500 focus:ring focus:ring-purple-500 focus:ring-opacity-50"
                         placeholder="Email"
+                        value={ contactForm.email.value }
+                        onChange={ ( e ) => handleChange( e, "email" ) }
                     />                    
                 </div>
                 <div className="flex grow p-2">
                     <textarea
-                        className="mt-1 w-full rounded-sm border-gray-300 shadow-sm focus:border-purple-500 focus:ring focus:ring-purple-500 focus:ring-opacity-50"
+                        className="text-black mt-1 w-full rounded-sm border-gray-300 shadow-sm focus:border-purple-500 focus:ring focus:ring-purple-500 focus:ring-opacity-50"
                         placeholder="Message"
-    
+                        value={ contactForm.message.value }
+                        onChange={ ( e ) => handleChange( e, "message" ) }
                     />                      
                 </div>
                 <div>
                     <button
-                        type="button"
+                        type="submit"
                         className={`text-black bg-white mt-6 border rounded-xl border-gray-300 p-2 hover:bg-purple-500 hover:text-white`}
-                        // disabled={loading}
-                        // onClick={handleSignIn}
                     >
                         Submit Message
                     </button>   
